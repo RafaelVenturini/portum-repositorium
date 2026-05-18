b327ce124c69bbc6c6a9c52f3e977c7891c6cb9e
Rafael Venturini <rafaelventurinidipalma@gmail.com>
2026-05-18 20:09:53 -0300

    start command

---
+++ start.zsh
@@ -0,0 +1,11 @@
+echo "Installing deps..."
+pip install invoke dotenv
+echo "Running invoke automation..."
+invoke install
+echo ""
+echo ""
+echo ""
+echo "Ready to use!"
+echo "" 
+echo "" 
+echo "" 
