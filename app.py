47d77afe042eaeca3f0936d90e81ccc396dcb3cc
Rafael Venturini <rafaelventurinidipalma@gmail.com>
2026-05-19 08:39:04 -0300

    just making my life easier

---
+++ app.py
@@ -0,0 +1,46 @@
+import logging
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    # Configuracao recomendada para desenvolvimento:
+    # Mostra mensagens DEBUG e INFO no console
+    if app.debug:
+        app.logger.setLevel(logging.DEBUG)
+
+    # # ----------------------------------------------------------
+    # # Configuracao da aplicacao (variaveis de ambiente)
+    # # ----------------------------------------------------------
+    # from repository.ext.config import init_app as init_config
+    # init_config(app)
+    
+    # if test_config:
+    #     app.config.update(test_config)
+
+    # # ----------------------------------------------------------
+    # # Inicializacao do banco de dados
+    # # ----------------------------------------------------------
+    # from repository.ext.db import init_app as init_db
+    # init_db(app)
+
+    # # Registro dos modelos no metadata do SQLAlchemy
+    # from repository.ext.db import register_models
+    # register_models()
+
+    # # ----------------------------------------------------------
+    # # Outras extensoes
+    # # ----------------------------------------------------------
+    # if not app.config.get("TESTING"):
+    #     from repository.ext.wtf import init_app as init_wtf
+    #     init_wtf(app)
+
+    # from repository.ext.debugtoolbar import init_app as init_toolbar
+    # init_toolbar(app)
+
+    # # ----------------------------------------------------------
+    # # Blueprints (camada de apresentacao)
+    # # ----------------------------------------------------------
+    # from repository.views import init_app as init_site
+    # init_site(app)
+    
+    return app
