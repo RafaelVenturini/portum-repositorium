2b5d81ae0441e7ab9bfb1b007c08ffbed5f1b112
Rafael Venturini <rafaelventurinidipalma@gmail.com>
2026-05-31 16:25:28 -0300

    moved delivery

--- delivery-base/delivery/__init__.py
+++
@@ -1,3 +0,0 @@
-from importlib import import_module
-
-create_app = import_module("app").create_app
--- delivery-base/delivery/ext/debugtoolbar/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_debugtoolbar import DebugToolbarExtension
-
-toolbar = DebugToolbarExtension()
-
-
-def init_app(app):
-    toolbar.init_app(app)
