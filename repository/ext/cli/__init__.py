333128a6df6dd0cd32f44685ece0219da7f04e69
Rafael Venturini <rafaelventurinidipalma@gmail.com>
2026-06-01 23:11:11 -0300

    cli seeder

--- repository/ext/cli/__init__.py
+++ repository/ext/cli/__init__.py
@@ -1,9 +1,10 @@
 import click
+
 from repository.ext.db import db
+from repository.ext.db.seed import seed_db
 
 
 def init_app(app):
-
     @app.cli.command("create-db")
     def create_db():
         """Cria as tabelas do db."""
@@ -41,3 +42,19 @@ def init_app(app):
         articles = scrape_porto_central(limit=limit, page=page, order=order)
         created, updated = save_articles(articles)
         click.echo(f"Scraping finalizado: {created} criadas, {updated} atualizadas.")
+
+    @app.cli.command("seed-db")
+    def seed_db_cli():
+        """Popula o banco de dados."""
+        seed_db()
+
+        print("Seed executado com sucesso.")
+
+    @app.cli.command("reset-database")
+    def reset_database():
+        """Reconstroi todo o banco de dados para debug"""
+
+        db.drop_all()
+        db.create_all()
+        seed_db()
+        print("Banco recriado com sucesso!")
